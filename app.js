17e73372f88e39a94c91ddfcd106af6fe9b99a23
XanderBits <amarettosyntax@protonmail.com>
2022-04-09 22:07:34 -0400

    [FIX]

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const express = require('express')
 const app = express();
 const {dbConnect} = require('./config/mongo.js')
 
-const PORT = process.env.PORT || 3000
 
-app.use(require('./app/routes'))
 app.use(express.json())
 app.use(express.urlencoded({extended : true}));
+app.use(require('./app/routes'))
 
 dbConnect()
+
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
